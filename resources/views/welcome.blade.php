b06fa723996797c7f8923524cd8ed6b9b399e271
Hieu Do <hieu.do@sotatek.com>
2019-07-10 22:46:38 +0700

    init api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,92 +8,44 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.css">
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <div class="container">
+      <h1>phone book</h1>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+      <a class="btn btn-primary btn-nueva" href="d"><b>+</b> Add contact</a>
+      <table class="table table-bordered grocery-crud-table table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone number</th>
+            <th>Email</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody class="table-body"> 
+        </tbody>
+      </table>
+    </div>
     </body>
+
+    <script type="text/javascript">
+        $.get('/api/contacts', function( contacts ) {
+          contacts.forEach((contact) => {
+            $('.table-body').append(`
+              <tr>
+              <td>${contact.name}</td>
+              <td>${contact.number}</td>
+              <td>${contact.email}</td>
+              <td>
+              <a class="btn btn-default btn-outline-dark" href=""> Edit </a>
+              <a class="btn btn-default btn-outline-dark" href=""> Delete </a>
+              </td>
+              </tr>`);
+          });
+        });
+    </script>
 </html>
